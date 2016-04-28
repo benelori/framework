d37b24ce705f9a78e8cddc68859501e5aee3f0a4
Bene Lorant <lbene@pitechplus.com>
2016-04-28 16:46:19 +0300

    Refactored code and separated concerns.

--- src/app.php
+++ src/app.php
@@ -1,28 +1,6 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-
-class LeapYearController
-{
-  public function indexAction($year)
-  {
-    if (is_leap_year($year)) {
-      return new Response('Yep, this is a leap year!');
-    }
-
-    return new Response('Nope, this is not a leap year.');
-  }
-}
-
-function is_leap_year($year = null) {
-  if (null === $year) {
-    $year = date('Y');
-  }
-
-  return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
@@ -30,7 +8,7 @@ $routes->add('bye', new Routing\Route('/bye'));
 
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
   'year' => null,
-  '_controller' => 'LeapYearController::indexAction',
+  '_controller' => 'Calendar\\Controller\\LeapYearController::indexAction',
 )));
 
 return $routes;
