ec746e6f406244a7d56299d3d9c162781b0d14d9
Bene Lorant <bene.lori89@gmail.com>
2016-05-01 22:43:39 +0300

    Added support for Drupal 8-like DI for Controllers.

--- src/Calendar/Controller/LeapYearController.php
+++ src/Calendar/Controller/LeapYearController.php
@@ -2,19 +2,35 @@
 
 namespace Calendar\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Calendar\Service\LeapYearManager;
+use Simplex\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Calendar\Model\LeapYear;
 
-class LeapYearController
-{
-  public function indexAction(Request $request, $year)
+class LeapYearController extends ControllerBase {
+
+  /**
+   * @var \Calendar\Service\LeapYearManager
+   */
+  protected $leapYearManager;
+
+  public function __construct(LeapYearManager $leapYearManager) {
+    $this->leapYearManager = $leapYearManager;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('calendar.leap_year_manager')
+    );
+  }
+  
+  public function indexAction($year)
   {
-    $leapyear = new LeapYear();
-    if ($leapyear->isLeapYear($year)) {
+    if ($this->leapYearManager->isLeapYear($year)) {
       return new Response('Yep, this is a leap year!');
     }
 
     return new Response('Nope, this is not a leap year.');
   }
+  
 }
